6055f08b1747fcc77fc84df49600f31add3fbed4
Paul Gierz <pgierz@awi.de>
2021-09-20 09:53:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ["esm_parser @ git+https://github.com/esm-tools/esm_parser.git",
+requirements = ["esm_tools @ git+https://github.com/esm-tools/esm_tools.git",
+                "esm_parser @ git+https://github.com/esm-tools/esm_parser.git",
                 "esm_runscripts @ git+https://github.com/esm-tools/esm_runscripts.git",
                 "colorama",
                 "coloredlogs",
